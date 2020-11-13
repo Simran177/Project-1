d934da5751f80931871cf8db95944ee4b8043dc2
Simran <simran.maurya_cs18@gla.ac.in>
2020-11-13 22:06:22 +0530

    dailydiet

---
+++ dailydiet.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daily Diet</title>
+  <!--Header Link-->
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <!--Link for thumbnails-->
+  <link rel="stylesheet" href="css/lightbox.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/cycle2.js"></script>
+	<script type="text/javascript" src="js/declarativeToggle.js"></script>
+  <script type="text/javascript" src="js/lightbox.min.js"></script>
+  <script type="text/javascript" src="js/testimonialjs.js"></script>
+  <!--For footer-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!--nav bar-->
+  <!--<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet"> -->
+	<link rel="stylesheet" href="css/dailydiet.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  
+</head>
+<body>
+  <div id="bar">
+    Daily Diet Recommneded.
+  </div>
+
+  <nav class="navigation" style="position: fixed; top: 50px; width: 100%;">
+    <div class="nav-logo"><img src="images/logo.jpg" alt="_target" style="line-height: 50px;width: 40px;height: 40px; margin-right: 15px;">HEALTH HUNT</div>
+    <ul class="nav-menu">
+      <li><a href="homepage.php">Home</a></li>
+      <li><a href="contactUs.php">Contact Us</a></li>
+      <li><a href="testimonial.php">Testimonials</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="signup.php"> Sign Up</a> </li>
+      
+    </ul>
+    <div class="nav-toggle">
+      <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+    </div>
+  </nav>
+  <div class="nav-overlay"></div>
+  
+  <script src="js/dailydiet.js"></script>
+
+<br><br><br>
+
+  <div id="heading">
+    <h1 id="head">Why This Food? &#129300</h1>
+    <h4>Just click on the images below to be apprised of the health benefits</h4>
+  </div>
+  <div class="content-area group">
+  <div class="main-area" style="margin-bottom: 100px;">
+    <div class="thumbnails group">
+        <a href="img/1.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/1.png" alt="_blank"></a>
+        <a href="img/2.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/2.jpg" alt="_blank"></a>
+        <a href="img/3.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/3.jpg" alt="_blank"></a>
+        <a href="img/4.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/4.jpg" alt="_blank"></a>
+        <a href="img/5.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/5.jpg" alt="_blank"></a>
+        <a href="img/6.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/6.jpg" alt="_blank"></a>
+        <a href="img/7.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/7.jpg" alt="_blank"></a>
+        <a href="img/8.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/8.jpg" alt="_blank"></a>
+        <a href="img/9.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/9.jpg" alt="_blank"></a>
+        <a href="img/10.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/10.jpg" alt="_blank"></a>
+        <a href="img/11.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/11.jpg" alt="_blank"></a>
+        <a href="img/12.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/12.jpg" alt="_blank"></a>
+        <a href="img/13.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/13.jpg" alt="_blank"></a>
+        <a href="img/14.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/14.jpg" alt="_blank"></a>
+        <a href="img/15.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/15.jpg" alt="_blank"></a>
+        <a href="img/16.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/16.jpg" alt="_blank"></a>
+        <a href="img/17.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/17.jpg" alt="_blank"></a>
+        <a href="img/18.jpg" data-lightbox="gallery" data-title="Picture"><img src="img/18.jpg" alt="_blank"></a>
+    </div>
+    
+    </div>
+    
+</div>
+
+<div id="#note">
+<h2 style="color: white; text-align: center; font-weight: bold; font-size: x-larger; ">Eat Well, Live Well !</h2>
+<p style="color: white; text-align: center; font-weight: 700; font-size: large;">We won't stop until we give you the best.</p>
+</div>
+  
+   
+
+    <!--Footer Code-->
+
+   <footer class="page-footer font-small stylish-color-dark pt-4" style="float: left; text-align: center; width: 100vw; background-color: #131418">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+  
+      <!-- Grid row -->
+      <div class="row">
+  
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3" style="color: white; text-align: left;">
+  
+          <!-- Content -->
+          <h3 class="text-uppercase" >Health Hunt</h3>
+          <p style="color:white;">We do our best to let you reach & experience each corner of the world.</p>
+  
+        </div>
+        <!-- Grid column -->
+        
+        <!--<hr class="clearfix w-100 d-md-none pb-3">-->
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3" style="color: white; text-align: left;">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+  
+          <ul class="list-unstyled">
+            <li style="padding: 5px 0px;">
+              <a href="homepage.php">Home</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="login.php">Log In</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="signup.php">Sign Up</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="about.php">About Us</a>
+            </li>
+          </ul>
+  
+        </div>
+        <!-- Grid column -->
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3" style="color: white; text-align: left;">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase" >Links</h5>
+  
+          <ul class="list-unstyled" >
+            <li style="padding: 5px 0px;">
+              <a href="contact.php">Contact Us</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="testimonial.php">Testimonials</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="index.php">Health Hunt</a>
+            </li>
+          </ul>
+  
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row -->
+  
+    </div>
+    <!-- Footer Links -->
+  
+  <hr noshade>
+  <div class="container3">
+  
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-2">
+      <li class="list-inline-item">
+        <h3 class="mb-1" style="color: white;">Register for free</h3>
+      </li>
+      <li class="list-inline-item" style="color: white;">
+        <a href="signup.php" class="btn btn-outline-white btn-rounded" style="color: white; border-radius: 20px; padding: 8px 20px; background-color: rgb(253, 63, 5);">
+            Sign up</a>
+            /
+            <strong><a href="login.php" style="color:rgb(253, 63, 5) ; font: lighter; letter-spacing: 1.5px;">Login</a></strong>
+  
+      </li>
+    </ul>
+    <!-- Call to action -->
+  
+  </div>
+  
+  
+  <hr noshade>
+    <div class="container2">
+  
+     
+      <ul class="list-unstyled list-inline text-center">
+        <li class="list-inline-item" >
+          <a class="btn-floating btn-fb mx-1" href="https://www.facebook.com/" >
+            <i class="fa fa-facebook"> </i>
+          </a>
+        </li>
+  
+        <li class="list-inline-item">
+          <a class="btn-floating btn- mx-1" href="https://www.twitter.com/">
+            <i class="fa fa-twitter"> </i>
+          </a>
+        </li>
+  
+        <li class="list-inline-item">
+          <a class="btn-floating btn- mx-1" href="https://www.google.com/">
+            <i class="fa fa-google"> </i>
+          </a>
+        </li>
+  
+        <li class="list-inline-item">
+          <a class="btn-floating btn-in mx-1" href="https://www.instagram.com/">
+            <i class="fa fa-instagram" > </i>
+          </a>
+        </li>
+      
+       
+        <li class="list-inline-item">
+          <a class="btn-floating btn- mx-1" href="https://www.linkedin.com/">
+            <i class="fa fa-linkedin"> </i>
+          </a>
+        </li>    
+      </ul>
+    </div> 
+  
+     
+       <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="color: grey; background-color: rgb(29, 28, 28); margin-bottom: -10px; height: 60px; display: flex; align-content: center; justify-content: center; line-height: 30px;">© 2020 Copyright:
+      <a href="https://healthhunt.com/" style="color: white">HealthHunt.com</a>
+    </div>
+  </footer>
+  <!-- Footer -->
+
+</body>
+</html>
