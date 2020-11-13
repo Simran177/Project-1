5d728fefe8d27c8b448cbf9f7b5c495dfb8462d3
Simran <simran.maurya_cs18@gla.ac.in>
2020-11-13 22:06:56 +0530

    exercise

---
+++ exercises.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Latest compiled and minified CSS -->
+    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+    <!-- jQuery library -->
+    <!--<script src="https;//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    
+    <!--<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet"> -->
+    <link rel="stylesheet" href="css/dailydiet.css">
+    <!--<script src="js/dailydiet.js"></script>-->
+    <title>Exercises</title>    
+
+    <!--Header Link-->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <!--nav bar-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!--For footer-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        
+    
+    
+    <style>
+       
+        body{
+            background-image: none;
+            background-color: rgb(137,196,244,0.5);
+           /* margin-left: 20px;
+            margin-right: 20px;*/
+            font-family: 'Open Sans';
+          
+        }
+        .col-md-4 img{
+           height:190px;
+       }
+       .thumbnail{
+           height:200px;
+       }
+       .thumbnail img{
+        transition: all 0.2s ease-in-out;
+       }
+       .thumbnail img:hover{
+         border: 1px solid teal;
+         transform: scale(0.92);
+       }
+       .caption p{
+          color:olivedrab;
+          font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+          font-weight:25px;
+          font-size:20px;
+          text-align: justify;
+       }
+       .caption h3{
+           color:orangered;
+           font-weight: bolder;
+       }
+       #banner{
+            width: 100vw;
+            margin-bottom: 15px;
+            height: 150px;
+            padding:70px 20px;
+            padding-bottom: 120px;
+            color:white;
+            font-weight: bolder;
+            font-family: cursive;
+            font-size: 40px;
+            position: relative;
+            display: flex;
+            justify-items: center;
+            align-content: center;
+        }
+        #banner img{
+            height:100px;
+            width:440px;
+        }
+        #banner h1{
+            position:absolute;
+        }
+        
+        
+    </style>
+</head>
+<body>
+  <div id="bar">
+    Daily Diet Recommneded.
+  </div>
+  <nav class="navigation" style="position: fixed; top: 50px; width: 100%;">
+    <div class="nav-logo"><img src="images/logo.jpg" alt="_target" style="line-height: 50px;width: 40px;height: 40px; margin-right: 15px;">HEALTH HUNT</div>
+    <ul class="nav-menu">
+      <li><a href="homepage.php">Home</a></li>
+      <li><a href="contactUs.php">Contact Us</a></li>
+      <li><a href="testimonial.php">Testimonials</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="signup.php"> Sign Up</a> </li>
+      
+    </ul>
+    <div class="nav-toggle">
+      <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+    </div>
+  </nav>
+  <div class="nav-overlay"></div>
+  
+  <script src="js/dailydiet.js"></script>
+
+    
+    
+
+<br><br><br><br><br><br>
+
+<h4 style="text-align: center;"><b><q> Yoga is the journey of the self, through the self, to the self.</q></b> ~  The Bhagavad Gita &#128578</h4>
+    
+    <div class="container-fluid" style="width: 100%;padding: 30px;">
+    <div class="row text-center" id="head">
+        <div class="col-md-4 col-sm-6" >
+            <a href="#" class="thumbnail"><img src="img/21.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Katichakrasana</h3>
+                <p>Katichakrasana is good for relieving constipation as this yoga strengthens and improves the flexibility of the spine and waist. Good for arm and leg muscles.</p>
+                <a class="btn btn-primary btn-block" href="https://www.artofliving.org/in-en/standing-spinal-twist-katichakrasana">Read more...</a>  
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/22.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Bhujangasana</h3>
+                <p>This asana improves menstrual irregularities, elevates mood firms and tones the buttocks. Stimulates the heart and organs in the abdomen, like the kidneys.</p>
+                <a class="btn btn-primary btn-block" href="https://www.artofliving.org/in-en/yoga/yoga-poses/cobra-pose-bhujangasana">Read more...</a>   
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/23.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Kurmasana</h3>
+                <p>It stretches legs, back, shoulders and chest and lengthens the back muscles. Kurmasana improves the functions of the respiratory and digestive systems.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/practice/drawn-inside">Read more...</a>   
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="row text-center">
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/24.jpg" width=300px; alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Setu Bandha Sarvangasana</h3>
+                <p>Setu Bandha Sarvangasana stretches the chest, neck, spine, and hips. This asana strengthens the back, buttocks, and hamstrings which in turn helps in improves circulation of blood.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/bridge-pose">Read more...</a>    
+            </div><br>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/25.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Matsyasana</h3>
+                <p>It relieves respiratory problems as it encourages the right kind of breathing. It also tones the pituitary, parathyroid. It also makes the muscles in the back of neck stronger.</p>
+                <a class="btn btn-primary btn-block" href="https://www.artofliving.org/in-en/yoga/yoga-poses/fish-pose">Read more...</a>   
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/26.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Adho Mukha Svanasana</h3>
+                <p>It strengthens and tones the arms and legs. The asana lengthens and straightens the spine and is beneficial for relieving the pain in the upper, middle and lower back.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/downward-facing-dog">Read more...</a>   
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="row text-center">
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/27.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3> Pada Rajakapotasana</h3>
+                <p>It relieves back problems, especially sciatica. Pada Rajakapotasana strengthens the back. The deep stretch relieves stress and anxiety.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/one-legged-king-pigeon-pose">Read more...</a>    
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/28.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Balasana</h3>
+                <p>Balasana stretches and strengthens the muscle of hips. The asana helps in alleviating stress and fatigue by increasing blood circulation.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/child-s-pose">Read more...</a>  
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/29.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Gomukhasana</h3>
+                <p>Gomukhasana stimulates the kidneys and is helpful in relieving ailments like diabetes, high blood pressure, and sexual malfunction.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/cow-face-pose">Read more...</a>   
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="row text-center">
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/30.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Tadasana</h3>
+                <p>This posture strengthens thighs, knees, and ankles. With the increased strength, power, and mobility in the feet, legs, and hips.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/mountain-pose">Read more...</a>  
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/31.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Pavanamuktasana</h3>
+                <p>Pavanamuktasana strengthens the abdominal muscles and massages the intestines and internal organs of the digestive system.</p>
+                <a class="btn btn-primary btn-block" href="https://www.artofliving.org/us-en/wind-relieving-pose-pavanamuktasana">Read more...</a>  
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/32.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Ustrasana</h3>
+                <p>This asana reduces fat on thighs and strengthens the shoulders and back.It expands the abdominal region, improving digestion.</p>
+                <a class="btn btn-primary btn-block" href="https://www.yogajournal.com/poses/camel-pose">Read more...</a>    
+            </div>
+        </div>
+    </div>
+    <br><br>
+    <div class="row text-center">
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/33.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Vayu Nishkasana</h3>
+                <p>The spinal stretch, the stretch in your arms and legs, are significant. A pressure between vertebrae and joints becomes balanced.</p>
+                <a class="btn btn-primary btn-block" href="http://selfhealingonline.com/offer-item/wind-releasing-pose-vayu-nishkasana-7/">Read more...</a>   
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/34.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Trikonasana</h3>
+                <p>Trikonasana cures indigestion and gives flexibility to groins, hamstrings, and hips. It stimulates the function of the kidney.</p>
+                <a class="btn btn-primary btn-block" href="https://www.artofliving.org/in-en/yoga/yoga-poses/triangle-pose-trikonasana">Read more...</a>  
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <a href="#" class="thumbnail"><img src="img/35.jpg" alt="Responsive image"></a>
+            <div class="caption">
+                <h3>Sarvangasana</h3>
+                <p>This asana prevents thyroid problems and also ensures smooth blood flow to the heart without any strain by force of gravity. </p>
+                <a class="btn btn-primary btn-block" href="https://www.artofliving.org/in-en/yoga/yoga-poses/shoulder-stand-sarvangasana">Read more...</a>   
+            </div>
+        </div>
+    </div>
+    </div>
+    <br><br>
+    
+    <!--Footer Code-->
+
+   <footer class="page-footer font-small stylish-color-dark pt-4" style="float: left; text-align: center; width: 100vw; background-color: #131418">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+  
+      <!-- Grid row -->
+      <div class="row">
+  
+        <!-- Grid column -->
+        <div class="col-md-6 mt-md-0 mt-3" style="color: white; text-align: left;">
+  
+          <!-- Content -->
+          <h3 class="text-uppercase" >Health Hunt</h3>
+          <p style="color:white;">We do our best to let you reach & experience each corner of the world.</p>
+  
+        </div>
+        <!-- Grid column -->
+        
+        <!--<hr class="clearfix w-100 d-md-none pb-3">-->
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3" style="color: white; text-align: left;">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase">Links</h5>
+  
+          <ul class="list-unstyled">
+            <li style="padding: 5px 0px;">
+              <a href="homepage.php">Home</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="login.php">Log In</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="signup.php">Sign Up</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="about.php">About Us</a>
+            </li>
+          </ul>
+  
+        </div>
+        <!-- Grid column -->
+  
+        <!-- Grid column -->
+        <div class="col-md-3 mb-md-0 mb-3" style="color: white; text-align: left;">
+  
+          <!-- Links -->
+          <h5 class="text-uppercase" >Links</h5>
+  
+          <ul class="list-unstyled" >
+            <li style="padding: 5px 0px;">
+              <a href="contact.php">Contact Us</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="testimonial.php">Testimonials</a>
+            </li>
+            <li style="padding: 5px 0px;">
+              <a href="index.php">Health Hunt</a>
+            </li>
+          </ul>
+  
+        </div>
+        <!-- Grid column -->
+  
+      </div>
+      <!-- Grid row -->
+  
+    </div>
+    <!-- Footer Links -->
+  
+  <hr noshade>
+  <div class="container3">
+  
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-2">
+      <li class="list-inline-item">
+        <h3 class="mb-1" style="color: white;">Register for free</h3>
+      </li>
+      <li class="list-inline-item" style="color: white;">
+        <a href="signup.php" class="btn btn-outline-white btn-rounded" style="color: white; border-radius: 20px; padding: 8px 20px; background-color: rgb(253, 63, 5);">
+            Sign up</a>
+            /
+            <strong><a href="login.php" style="color:rgb(253, 63, 5) ; font: lighter; letter-spacing: 1.5px;">Login</a></strong>
+  
+      </li>
+    </ul>
+    <!-- Call to action -->
+  
+  </div>
+  
+  
+  <hr noshade>
+    <div class="container2">
+  
+     
+      <ul class="list-unstyled list-inline text-center">
+        <li class="list-inline-item" >
+          <a class="btn-floating btn-fb mx-1" href="https://www.facebook.com/" >
+            <i class="fa fa-facebook"> </i>
+          </a>
+        </li>
+  
+        <li class="list-inline-item">
+          <a class="btn-floating btn- mx-1" href="https://www.twitter.com/">
+            <i class="fa fa-twitter"> </i>
+          </a>
+        </li>
+  
+        <li class="list-inline-item">
+          <a class="btn-floating btn- mx-1" href="https://www.google.com/">
+            <i class="fa fa-google"> </i>
+          </a>
+        </li>
+  
+        <li class="list-inline-item">
+          <a class="btn-floating btn-in mx-1" href="https://www.instagram.com/">
+            <i class="fa fa-instagram" > </i>
+          </a>
+        </li>
+      
+       
+        <li class="list-inline-item">
+          <a class="btn-floating btn- mx-1" href="https://www.linkedin.com/">
+            <i class="fa fa-linkedin"> </i>
+          </a>
+        </li>    
+      </ul>
+    </div> 
+  
+     
+       <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="color: grey; background-color: rgb(29, 28, 28); margin-bottom: -10px; height: 60px; display: flex; align-content: center; justify-content: center; line-height: 30px;">© 2020 Copyright:
+      <a href="https://healthhunt.com/" style="color: white">HealthHunt.com</a>
+    </div>
+  </footer>
+  <!-- Footer -->
+
+    
+   
+  <!--<script>
+    var h= document.getElementById('head');
+    (function($){
+    $('.nav-toggle').click(function(e){
+        e.preventDefault();
+        $('.nav-menu').toggleClass('active');
+        h.style.marginTop = "150px";
+    })
+})
+(jQuery);
+</script>-->
+    </body>
+    </html>
